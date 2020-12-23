7886dbd1d34ea8f237da24e40352f2025d7ae50f
Alice <yi.yayu0106@gmail.com>
2020-12-23 22:41:59 +0800

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init //因為dist資料夾預設是被ignore的，因此在進入dist資料�
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/ya-yu0106/vue_webpack.git分支為 gh-pages
-git push -f git@github.com:ya-yu0106/vue_webpac.git master:gh-pages
+git push -f git@github.com:ya-yu0106/vue_webpack.git master:gh-pages
 //將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
